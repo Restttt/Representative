643039d3c7431ad902bf8ac1aac8c0576c3e6830
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-31 22:33:44 -0600

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,128 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Front End Challenge
+For this programming test you are tasked with creating an interface around 
+the open API located at http://whoismyrepresentative.com/api. 
+However, to get around CORs limitation, we are providing an extremely simple node application 
+that will act as a layer between your client app and the API.  If you want to and can get around the CORs limitation, 
+feel free to ignore the node server.  If you don't want the hassle, you can follow the instructions 
+below to get everything up and running. If you do, you will be using the node app as your API - you will not be accessing whoismyrepresentative.com directly from your client side app
 
-## Available Scripts
+In your client side application, you will be making use of two of the endpoints that are offered: 
 
-In the project directory, you can run:
+* /representatives/:state
+* /senators/:state
 
-### `npm start`
+Your application should allow the user to: 
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+* Select if he/she would like to search by Representative or by Senator
+* Select which state to return results for
+* View a results list after #1 and #2 have been selected
+* Display name and party affiliation
+* View more information about the representative/senator by clicking on his/her name in the results list (district,  phone, office and a link to his/her website)
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+Your website should be built using React and match the following design.
+Feel free to use whatever libraries you want to make the design fit.
 
-### `npm test`
+Priorities: 
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+1) Functionality
+2) Design
+3) Code organization
+4) Proper error handling (make sure they select something before clicking on the button to fetch results)
 
-### `npm run build`
+If you have any questions, please let us know. 
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![Example layout](example.png)
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+# find-rep-api
+Super simple api to get results from http://whoismyrepresentative.com/api
 
-### `npm run eject`
+#Installation
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+* Install node (v6+)
+* Run `npm install` or `yarn`
+* Run `npm start`
+* Enjoy immensely.
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+###Available endpoints: 
 
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+`/representatives/:state`
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+_:state_ must be a valid state abbreviation (UT, WY, MO...etc)
 
-## Learn More
+**response**
+```json
+{  
+   "success":true,
+   "results":[  
+      {  
+         "name":"Rob Bishop",
+         "party":"R",
+         "state":"UT",
+         "district":"1",
+         "phone":"202-225-0453",
+         "office":"123 Cannon House Office Building",
+         "link":"http://robbishop.house.gov"
+      },
+      {  
+         "name":"Jason Chaffetz",
+         "party":"R",
+         "state":"UT",
+         "district":"3",
+         "phone":"202-225-7751",
+         "office":"2464 Rayburn House Office Building",
+         "link":"http://chaffetz.house.gov"
+      },
+      {  
+         "name":"Jim Matheson",
+         "party":"D",
+         "state":"UT",
+         "district":"4",
+         "phone":"202-225-3011",
+         "office":"2211 Rayburn House Office Building",
+         "link":"http://matheson.house.gov"
+      },
+      {  
+         "name":"Chris Stewart",
+         "party":"R",
+         "state":"UT",
+         "district":"2",
+         "phone":"202-225-9730",
+         "office":"323 Cannon House Office Building",
+         "link":"http://stewart.house.gov"
+      }
+   ]
+}
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+`/senators/:state`
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+_:state_ must be a valid state abbreviation (UT, WY, MO...etc)
 
-### Code Splitting
+**response**
+```json
+{  
+   "success":true,
+   "results":[  
+      {  
+         "name":"Orrin Hatch",
+         "party":"R",
+         "state":"UT",
+         "district":"Senior Seat",
+         "phone":"202-224-5251",
+         "office":"104 Hart Senate Office Building",
+         "link":"http://www.hatch.senate.gov"
+      },
+      {  
+         "name":"Mike Lee",
+         "party":"R",
+         "state":"UT",
+         "district":"Junior Seat",
+         "phone":"202-224-5444",
+         "office":"316 Hart Senate Office Building",
+         "link":"http://www.lee.senate.gov"
+      }
+   ]
+}
+```
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
---
+++ src/components/Home/Home.js
@@ -0,0 +1,232 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Select from 'react-select';
+import swal from 'sweetalert';
+
+
+import './Home.scss';
+
+
+
+const option1 = [
+  { value: 'representative', label: 'Representative' },
+  { value: 'senator', label: 'Senator' }
+]
+
+const option2 = [
+  { value: "AL", label: 'Alabama' },
+  { value: "AK", label: 'Alaska' },
+  { value: "AZ", label: 'Arizona' },
+  { value: "AR", label: 'Arkansas' },
+  { value: "CA", label: 'California' },
+  { value: "CO", label: 'Colorado' },
+  { value: "CT", label: 'Connecticut' },
+  { value: "DE", label: 'Delaware' },
+  { value: "DC", label: 'District Of Columbia' },
+  { value: "FL", label: 'Florida' },
+  { value: "GA", label: 'Georgia' },
+  { value: "HI", label: 'Hawaii' },
+  { value: "ID", label: 'Idaho' },
+  { value: "IL", label: 'Illinois' },
+  { value: "IN", label: 'Indiana' },
+  { value: "IA", label: 'Iowa' },
+  { value: "KS", label: 'Kansas' },
+  { value: "KY", label: 'Kentucky' },
+  { value: "LA", label: 'Louisiana' },
+  { value: "ME", label: 'Maine' },
+  { value: "MD", label: 'Maryland' },
+  { value: "MA", label: 'Massachusetts' },
+  { value: "MI", label: 'Michigan' },
+  { value: "MN", label: 'Minnesota' },
+  { value: "MS", label: 'Mississippi' },
+  { value: "MO", label: 'Missouri' },
+  { value: "MT", label: 'Montana' },
+  { value: "NE", label: 'Nebraska' },
+  { value: "NV", label: 'Nevada' },
+  { value: "NH", label: 'New Hampshire' },
+  { value: "NJ", label: 'New Jersey' },
+  { value: "NM", label: 'New Mexico' },
+  { value: "NY", label: 'New York' },
+  { value: "NC", label: 'North Carolina' },
+  { value: "ND", label: 'North Dakota' },
+  { value: "OH", label: 'Ohio' },
+  { value: "OK", label: 'Oklahoma' },
+  { value: "OR", label: 'Oregon' },
+  { value: "PA", label: 'Pennsylvania' },
+  { value: "RI", label: 'Rhode Island' },
+  { value: "SC", label: 'South Carolina' },
+  { value: "SD", label: 'South Dakota' },
+  { value: "TN", label: 'Tennessee' },
+  { value: "TX", label: 'Texas' },
+  { value: "UT", label: 'Utah' },
+  { value: "VT", label: 'Vermont' },
+  { value: "VA", label: 'Virginia' },
+  { value: "WA", label: 'Washington' },
+  { value: "WV", label: 'West Virginia' },
+  { value: "WI", label: 'Wisconsin' },
+  { value: "WY", label: 'Wyoming' }
+]
+
+
+
+
+
+const Home = () => {
+
+  const [firstName, changeFirst] = useState('First Name');
+  const [lastName, changeLast] = useState('Last Name');
+  const [district, changeDistrict] = useState('District');
+  const [phone, changePhone] = useState('Phone');
+  const [office, changeOffice] = useState('Office');
+  const [officeType, changeType] = useState(null);
+  const [usState, changeUsState] = useState(null);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    return () => {
+      return (firstName, lastName, district, phone, office, usState, officeType, people);
+    };
+  }, [firstName, lastName, district, phone, office, officeType, usState, people]);
+
+
+
+
+  function search() {
+    if (officeType === null || usState === null) {
+      return swal("Oops!", "Select an office and state!", "error");
+    }
+
+    if (officeType.value === 'senator') {
+      Axios.get(`/senators/${usState.value}`).then(res => {
+        return setPeople(res.data.results);
+      }).catch(err => console.log(err));
+    } else if (officeType.value === 'representative') {
+      Axios.get(`/representatives/${usState.value}`).then(res => {
+        return setPeople(res.data.results);
+      }).catch(err => console.log(err));
+    } else {
+      return swal("Oops!", "Select an office and state!", "error");
+    }
+
+    changeType(null);
+    changeUsState(null);
+    changeFirst('First Name');
+    changeLast('Last Name');
+    changeDistrict('Disctrict');
+    changePhone('Phone');
+    changeOffice('Office');
+  }
+
+  function displayInfo(person) {
+
+    let firstName = person.name.split(' ').slice(0, -1).join(' ');
+    let lastName = person.name.split(' ').slice(-1).join(' ');
+
+    changeFirst(firstName);
+    changeLast(lastName);
+    changeDistrict(person.district);
+    changePhone(person.phone);
+    changeOffice(person.office);
+  }
+
+  
+  function displayParty() {
+    return people.map(person => {
+      if (person.party === "Democrat") {
+        return (
+          <li key={person.name} onClick={() => displayInfo(person)}>D</li>
+        )
+      } else if (person.party === "Republican") {
+        return (
+          <li key={person.name} onClick={() => displayInfo(person)}>R</li>
+        )
+      } else if (person.party === "Independent") {
+        return (
+          <li key={person.name} onClick={() => displayInfo(person)}>I</li>
+        )
+      } else {
+        return (
+          <li key={person.name} onClick={() => displayInfo(person)}>{person.party}</li>
+        )
+      }
+    })
+  }
+
+  function displayName() {
+    return people.map(person => {
+      return (
+        <li key={person.name} onClick={() => displayInfo(person)}>{person.name}</li>
+      )
+    })
+  }
+
+
+
+  return (
+    <div className="home">
+
+      <header>
+        <h1>Who's My Representative?</h1>
+      </header>
+
+
+      <div className="page-info">
+
+        <div className="selection-container">
+          <Select
+            value={officeType}
+            onChange={changeType}
+            options={option1} x
+            className="select"
+          />
+          <Select
+            value={usState}
+            onChange={changeUsState}
+            options={option2}
+            className="select"
+          />
+          <button onClick={search}>Search</button>
+        </div>
+
+        <div className="result-container">
+
+          <div className="list-container">
+
+            <div className="list">
+              <h2>List  /  <span>Represenatitives</span></h2>
+              <div className="data">
+                <div>
+                  <h3>Name</h3>
+                  <ul>
+                    {displayName()}
+                  </ul>
+                </div>
+                <div>
+                  <h3>Party</h3>
+                  <ul>
+                    {displayParty()}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="info-container">
+            <h2>Info</h2>
+            <input value={firstName} readOnly="readOnly" />
+            <input value={lastName} readOnly="readOnly" />
+            <input value={district} readOnly="readOnly" />
+            <input value={phone} readOnly="readOnly" />
+            <input value={office} readOnly="readOnly" />
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Home;
